65325334495b494ba4b9ba67d47494d4d439ee37
Lolster24Vito <vitomir.hardi@gmail.com>
2025-01-21 17:58:57 +0100

    First commit

---
+++ src/test/java/com/company/website/companywebsite/CompanyWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.website.companywebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyWebsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
